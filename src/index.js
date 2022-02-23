d3b734ea6632b3de46937794113bd4267b48da12
Natalia Lankevich <nataliiks@rambler.ru>
2022-02-23 22:18:14 +0300

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+
+
+if (arguments.length === 0) {
+    return []
+}
+
+if (Array.isArray(matrix)){
+    if (matrix.length === 0) {
+        return []
+    }
+}
+
+let res =[];
+if (matrix.length>0 && arguments.length>0){
+
+    matrix.forEach((item, index) =>{
+         if (index % 2 === 0){
+             for (let i = 0; i < item.length; i++){
+                 res.push(item[i]);
+             }
+         } else {
+             for (let j = (item.length-1); j >=0; j--){
+                 res.push(item[j]);
+             }
+         } 
+    }); 
+
+
+} return res;
 }
